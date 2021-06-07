53e4dc44db219cdb0ee4a5cabfe89d8f33960604
Nithin Sai K J <nithins674@gmail.com>
2021-06-07 17:46:45 +0530

    added basic google login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
+
+const responseGoogle = (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+  console.log(response);
+}
 
 function App() {
   useEffect(() => {
@@ -8,8 +13,13 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <BrowserRouter>
-      </BrowserRouter>
+      <GoogleLogin
+        clientId="422317457275-32trbdq50ku5hu36qr13b0oa9tb0cs8o.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
