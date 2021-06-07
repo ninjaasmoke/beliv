d592552c87e522024165c91d2e64b4389f29bb4e
Nithin Sai K J <nithins674@gmail.com>
2021-06-07 21:28:26 +0530

    Terms & Policies

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 import { PrivateRoute } from './components/PrivateRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
 
 function App() {
   useEffect(() => {
@@ -14,6 +16,8 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/login" component={Login} />
+          <Route path="/privacy" component={Privacy} />
+          <Route path="/terms" component={Terms} />
           <PrivateRoute path="/" exact component={Home} />
         </Switch>
       </BrowserRouter>
