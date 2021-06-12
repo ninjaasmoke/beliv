ce1953ced5480cb22702bea08ff122ce8bb6ee24
Nithin Sai K J <nithins674@gmail.com>
2021-06-12 16:00:04 +0530

    ui for room

--- src/components/NewRoom.tsx
+++ src/components/NewRoom.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAuthContext } from '../context/AuthContext'
 
 const NewRoom: React.FC = () => {
+    const { userData } = useAuthContext();
+
     const style: React.CSSProperties = {
         borderRadius: 8,
         backgroundColor: 'var(--sec)',
@@ -13,7 +16,7 @@ const NewRoom: React.FC = () => {
         color: 'var(--color)'
     }
     return (
-        <Link to={'/room/roomID'}>
+        <Link to={'/room/' + userData.googleId}>
             <div className="newRoom" style={style}>
                 Create new room &nbsp;+
             </div>
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { RouteComponentProps } from 'react-router-dom';
+import '../styles/Room.css'
+
+import copy from '../assets/images/copy.svg'
 
 interface RoomParams {
     roomID: string
@@ -9,8 +12,17 @@ const Room: React.FC<RouteComponentProps<RoomParams>> = ({ match }) => {
     const { roomID } = match.params;
     console.log(roomID)
     return (
-        <div>
-            <h1>Room {match.params.roomID} </h1>
+        <div className="room">
+            <div className="roomMain">
+                <h1>Main</h1>
+            </div>
+            <div className="roomSideBar">
+                <div className="copy">
+                    <h5>Copy Room ID</h5>
+                    <span>{match.params.roomID}</span>
+                    <button onClick={() => { navigator.clipboard.writeText(match.params.roomID) }} title="Copy Room ID"> <img src={copy} alt="Copy" /> </button>
+                </div>
+            </div>
         </div>
     )
 }
