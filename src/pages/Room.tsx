300283924041eeb295202709a49a7cdc9bfd2b4c
Nithin Sai K J <nithins674@gmail.com>
2021-06-13 00:46:04 +0530

    removing err

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -28,10 +28,8 @@ const Room: React.FC<RouteComponentProps<RoomParams>> = ({ match }) => {
             setMessages && setMessages((msgs: any) => [...msgs, { ...m, sent: true }]);
             input.current?.value && (input.current.value = "");
             const lastTop = document.getElementById('lastMsg')?.offsetTop;
-            if (lastTop) {
+            if (lastTop)
                 document.getElementById('messages')?.scrollTo(0, lastTop);
-                document.getElementById('sendChat')?.focus()
-            }
         }
     }
 
